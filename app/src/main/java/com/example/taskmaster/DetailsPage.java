53cff752e1a9b5aa4969bec62642f6283e1ddb01
ahren <ahrenswett@gmail.com>
2019-10-23 13:53:39 -0700

    added detaoils page added username input

---
+++ app/src/main/java/com/example/taskmaster/DetailsPage.java
@@ -0,0 +1,18 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_page);
+        TextView title = getIntent().getStringExtra("task");
+
+    }
+}
